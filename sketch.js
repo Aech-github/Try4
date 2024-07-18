38854d8724d38f9bc0232a4fb361647a65c23063
Aech-github <nikhil.shah.sod23@anu.edu.in>
2024-07-18 16:02:09 +0530

    done

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(600, 600);
+  background(300,150,0);
+  strokeWeight(10);
+  stroke('white');
+ 
+
+}
+
+function draw() {
+ // declare variables 
+  let posX = mouseX;
+  let PosY = mouseY;
+  // place a dot on the screen
+   point( posX,PosY);
+  
+}
